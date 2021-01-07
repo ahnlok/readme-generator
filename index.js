85652f09c6046bbae5ee1d3c1545810b1d242aef
Sungpil An <ansungpil1@gmail.com>
2021-01-07 14:52:10 -0500

    Added array of questions and return function in index.js

--- index.js
+++ index.js
@@ -2,8 +2,45 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
-
+// const questions = [];
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is the description for your project?",
+        name: "description",
+    },  
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username",
+    },  
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },  
+    {
+        type: "list",
+        message: "What licesne do you like to use?",
+        name: "license",
+        choices: ["MIT", "ISC", "Microsoft Public License", "Open Software License", "Mozilla Public License"]
+    },
+])
+.then((response) => {
+    console.log(response);
+    fs.appendFile("user.json", JSON.stringify(response), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Your README choices is saved");
+        }
+    });
+});
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
