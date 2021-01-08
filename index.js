119ee14dc2d9098713b553542fb3e21f899272aa
Sungpil An <ansungpil1@gmail.com>
2021-01-08 12:16:22 -0500

    Added init function in index.js

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+
+// License Variables
+const apache = "Licensed under the [Apache License](https://spdx.org/licenses/Apache-2.0.html).";
+const gnu    = "Licensed under the [GNU GPLv3 License](https://spdx.org/licenses/GPL-3.0-or-later.html).";
+const mit    = "Licensed under the [MIT License](https://spdx.org/licenses/MIT.html).";
+const isc    = "Licensed under the [ISC License](https://spdx.org/licenses/ISC.html).";
+
 // TODO: Create an array of questions for user input
 // const questions = [];
 const questions = [
@@ -31,22 +38,22 @@ const questions = [
         name: "license",
         choices: ["MIT", "ISC", "Microsoft Public License", "Open Software License", "Mozilla Public License"]
     },
-]
-.then((response) => {
-    console.log(response);
-    fs.appendFile("user.json", JSON.stringify(response), (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Your README choices is saved");
-        }
+];
+// function to write new ReadMe file
+function init() {
+    inquirere.prompt(questions).then((reponse) => {
+        console.log(response);
+
+        const dynamicString = generateMarkdown(response);
+        
+        fs.writeFile("goodreadme.md", dynamicString, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Successfully generated new README.md file");
+            }
+        });
     });
-});
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 init();
